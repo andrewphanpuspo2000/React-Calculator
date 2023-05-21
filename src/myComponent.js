1886694f1a5787f78b1d2bc0f5e66d99984e69fa
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-05-21 16:06:03 +1000

    react-first

---
+++ src/myComponent.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export const MyComponent = () => {
+  return <h1>Hello Component</h1>;
+};
+const YourComponent = () => {
+  return <h3>Hello Component</h3>;
+};
+
+export default YourComponent;
